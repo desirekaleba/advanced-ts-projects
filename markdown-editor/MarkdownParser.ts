ae2a882f69993b91304686022fb80f225860647b
desirekaleba <desirekaleba@gmail.com>
2021-01-16 23:48:32 +0300

    put everything together with chain-of-responsibility pattern

--- markdown-editor/MarkdownParser.ts
+++ markdown-editor/MarkdownParser.ts
@@ -1,15 +1,22 @@
 class HtmlHandler {
+    private markdownChange: Markdown = new Markdown;
     public TextChangeHandler(inputId: string, outputId: string): void {
         let markdown = <HTMLTextAreaElement>document.getElementById(inputId);
         let markdownOutput = <HTMLLabelElement>document.getElementById(outputId);
         if (markdown !== null) {
             markdown.onkeyup = (e) => {
-                if (markdown.value)
-                    markdownOutput.innerHTML = markdown.value;
-                else
-                    markdownOutput.innerHTML = `<p></p>`;
+                this.RenderHtmlContent(markdown, markdownOutput);
+            }
+            window.onload = (e) => {
+                this.RenderHtmlContent(markdown, markdownOutput);
             }
         }
+    }
+    private RenderHtmlContent(markdown: HTMLTextAreaElement, markdownOutput: HTMLLabelElement) {
+        if (markdown.value)
+            markdownOutput.innerHTML = this.markdownChange.ToHtml(markdown.value);
+        else
+            markdownOutput.innerHTML = `<p></p>`;
     }
 }
 
@@ -87,12 +94,12 @@ interface IVisitable {
 }
 
 abstract class VisitorBase implements IVisitor {
-    constructor(private readonly tagType: TagType, private readonly TagTypeToHtml: TagTypeToHtml){
+    constructor(private readonly tagType: TagType, private readonly TagTypeToHtml: TagTypeToHtml) {
 
     }
     Visit(token: ParseElement, markdownDocument: IMarkdownDocument): void {
         markdownDocument.Add(this.TagTypeToHtml.OpeningTag(this.tagType), token.CurrentLine,
-        this.TagTypeToHtml.ClosingTag(this.tagType));
+            this.TagTypeToHtml.ClosingTag(this.tagType));
     }
 }
 
@@ -125,5 +132,121 @@ class HorizontalRuleVisitor extends VisitorBase {
 class Visitable implements IVisitable {
     Accept(visitor: IVisitor, token: ParseElement, markdownDocument: IMarkdownDocument): void {
         visitor.Visit(token, markdownDocument);
+    }
+}
+
+// deciding which tags to use with chain-of-responsibility pattern
+abstract class Handler<T> {
+    protected next: Handler<T> | null = null;
+    public SetNext(next: Handler<T>): void {
+        this.next = next;
+    }
+    public HandleRequest(request: T): void {
+        if (!this.CanHandle(request)) {
+            if (this.next !== null) {
+                this.next.HandleRequest(request);
+            }
+            return;
+        }
+    }
+    protected abstract CanHandle(request: T): boolean;
+}
+
+class ParseChainHandler extends Handler<ParseElement> {
+    private readonly visitable: IVisitable = new Visitable();
+    constructor(private readonly document: IMarkdownDocument,
+        private readonly tagType: string,
+        private readonly visitor: IVisitor) {
+        super();
+    }
+    protected CanHandle(request: ParseElement): boolean {
+        let split = new LineParser().Parse(request.CurrentLine, this.tagType);
+        if (split[0]) {
+            request.CurrentLine = split[1];
+            this.visitable.Accept(this.visitor, request, this.document);
+        }
+        return split[0];
+    }
+}
+
+// a class to parse our md
+class LineParser {
+    public Parse(value: string, tag: string): [boolean, string] {
+        let output: [boolean, string] = [false, ""];
+        output[1] = value;
+        if (value === "") {
+            return output;
+        }
+        let split = value.startsWith(`${tag}`);
+        if (split) {
+            output[0] = true;
+            output[1] = value.substr(tag.length);
+        }
+        return output;
+    }
+}
+// class to handle paragraphs
+class ParagraphHandler extends Handler<ParseElement> {
+    private readonly visitable: IVisitable = new Visitable();
+    private readonly visitor: IVisitor = new ParagraphVisitor();
+    protected CanHandle(request: ParseElement): boolean {
+        this.visitable.Accept(this.visitor, request, this.document);
+        return true;
+    }
+    constructor(private readonly document: IMarkdownDocument) {
+        super();
+    }
+}
+
+// handlers for appropriate tags
+class Header1ChainHandler extends ParseChainHandler {
+    constructor(document: IMarkdownDocument) {
+        super(document, "# ", new Header1Visitor());
+    }
+}
+class Header2ChainHandler extends ParseChainHandler {
+    constructor(document: IMarkdownDocument) {
+        super(document, "## ", new Header2Visitor());
+    }
+}
+class Header3ChainHandler extends ParseChainHandler {
+    constructor(document: IMarkdownDocument) {
+        super(document, "### ", new Header3Visitor());
+    }
+}
+class HorizontalRuleHandler extends ParseChainHandler {
+    constructor(document: IMarkdownDocument) {
+        super(document, "---", new HorizontalRuleVisitor());
+    }
+}
+// build the chain
+class ChainOfResponsibilityFactory {
+    Build(document: IMarkdownDocument): ParseChainHandler {
+        let header1: Header1ChainHandler = new Header1ChainHandler(document);
+        let header2: Header2ChainHandler = new Header2ChainHandler(document);
+        let header3: Header3ChainHandler = new Header3ChainHandler(document);
+        let horizontalRule: HorizontalRuleHandler = new HorizontalRuleHandler(document);
+        let paragraph: ParagraphHandler = new ParagraphHandler(document);
+
+        header1.SetNext(header2);
+        header2.SetNext(header3);
+        header3.SetNext(horizontalRule);
+        horizontalRule.SetNext(paragraph);
+
+        return header1;
+    }
+}
+
+class Markdown {
+    public ToHtml(text: string): string {
+        let document: IMarkdownDocument = new MarkdownDocument();
+        let header1: Header1ChainHandler = new ChainOfResponsibilityFactory().Build(document);
+        let lines: string[] = text.split(`\n`);
+        for (let index = 0; index < lines.length; index++) {
+            let parseElement: ParseElement = new ParseElement();
+            parseElement.CurrentLine = lines[index];
+            header1.HandleRequest(parseElement);
+        }
+        return document.Get();
     }
 }
